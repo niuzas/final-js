610fc8cbeb42ed75d6f0db563bd4755ce4ef8c85
dariusra2 <darius@nite.lt>
2021-03-31 11:01:31 +0300

    fix: user edit fixed

---
+++ client/src/components/FormCreateUser/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Button from '../Button';
+import FormGroup from '../FormGroup';
+import styles from './styles.module.css';
+
+class FormCreateUser extends React.Component {
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleSubmit();
+  }
+
+  render() {
+    const actionTitle = (this.props.isUpdating ? 'Update' : 'Create') + ' User';
+    return (
+      <form className={styles.FormCreate} onSubmit={this.handleFormSubmit}>
+        <h2>{actionTitle}</h2>
+        <div className={styles.FormGroupContainer}>
+          <FormGroup
+            name="name"
+            type="text"
+            value={this.props.inputData.name ?? ''}
+            handleChange={(value) => this.props.handleInputChange({ name: value })}
+          />
+          <FormGroup
+            name="age"
+            type="number"
+            value={this.props.inputData.age ?? ''}
+            handleChange={(value) => this.props.handleInputChange({ age: value })}
+          />
+          <FormGroup
+            name="email"
+            type="text"
+            value={this.props.inputData.email ?? ''}
+            handleChange={(value) => this.props.handleInputChange({ email: value })}
+          />
+          <FormGroup
+            name="password"
+            type="password"
+            value={this.props.inputData.password ?? ''}
+            handleChange={(value) => this.props.handleInputChange({ password: value })}
+          />
+          <Button>{actionTitle}</Button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default FormCreateUser;
